f60373197de732b472d3bb3f491d42cc4a7b41a0
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-02-22 20:18:54 +0530

    Update ec2main.tf

--- day-3(custom-nw)/ec2main.tf
+++ day-3(custom-nw)/ec2main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "dev" {
     ami = var.ami
     instance_type = var.instance_type
     key_name = var.key_name
-    subnet_id = aws_subnet.dev_subnet.id
+    subnet_id = aws_subnet.dev.id
     tags = {
       Name = "dev-ec2"
     }

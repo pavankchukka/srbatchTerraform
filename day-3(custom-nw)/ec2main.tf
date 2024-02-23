ad1ae26e9fad2cad9f3943e0de59d5fa447906ee
CloudTechDevOps <cloud87777@gmail.com>
2024-02-23 20:27:13 +0530

    updates

--- day-3(custom-nw)/ec2main.tf
+++ day-3(custom-nw)/ec2main.tf
@@ -1,8 +1,8 @@
 # create instance 
+
 resource "aws_instance" "dev" {
     ami = var.ami
     instance_type = var.instance_type
-    key_name = var.key_name
     subnet_id = aws_subnet.dev.id
     tags = {
       Name = "dev-ec2"

a6833b2a90f694f0beaaf32549f1bbbb28348cdf
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-02-23 20:37:29 +0530

    Update backend.tf

--- day-4-s3backend/backend.tf
+++ day-4-s3backend/backend.tf
@@ -1,7 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket = "day4s3create"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
+
+# This backend configuration instructs Terraform to store its state in an S3 bucket.
+ terraform {
+backend "s3" {
+    bucket         = "day4s3create"  # Name of the S3 bucket where the state will be stored.
+    key            = "terraform.tfstate" # Path within the bucket where the state will be read/written.
+    dynamodb_table = "terraform-lock" # DynamoDB table used for state locking.
+    encrypt        = true  # Ensures the state is encrypted at rest in S3.
+}
 }

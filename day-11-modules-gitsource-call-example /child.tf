5f5bb50b7ec3447785f8494ea720350af2fcdf8d
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-02-27 20:24:28 +0530

    Update child.tf

--- day-11-modules-gitsource-call-example /child.tf
+++ day-11-modules-gitsource-call-example /child.tf
@@ -1,5 +1,5 @@
 module "test" {
-    source = "github.com/CloudTechDevOps/Terraform/day-11-modules_root_child/modules/ec2" # here i am inserting values by calling day-11-modules_root_child 
+    source = #"github.com/CloudTechDevOps/Terraform/day-11-modules_root_child/modules/ec2" # here i am inserting values by calling day-11-modules_root_child 
     ami="ami-0440d3b780d96b29d"
     instance_type="t2.micro"
     
